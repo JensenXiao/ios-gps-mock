dd268f2d8539a1bf32069d10d1064d4d41193011
Mason Yen <t23133256@gmail.com>
2026-03-02 11:22:12 +0800

    Initial Commit

---
+++ O.PaperclipTests/O_PaperclipTests.swift
@@ -0,0 +1,16 @@
+//
+//  O_PaperclipTests.swift
+//  O.PaperclipTests
+//
+//  Created by Mason Yen on 3/2/26.
+//
+
+import Testing
+
+struct O_PaperclipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
